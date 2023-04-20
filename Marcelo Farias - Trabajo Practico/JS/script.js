d1dbccb19b9228100d7321bcad73a49979e6aa2f
KefinexAtto <131406279+KefinexAtto@users.noreply.github.com>
2023-04-20 19:19:25 -0300

    Delete Marcelo Farias - Trabajo Practico directory

--- Marcelo Farias - Trabajo Practico/JS/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarTexto(id) {
-    var texto = document.getElementById(id);
-    if (texto.style.display === "none") {
-      texto.style.display = "block";
-    } else {
-      texto.style.display = "none";
-    }
-  }
